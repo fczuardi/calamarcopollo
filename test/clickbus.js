d1c49af4ee6b171b22739bf2e98ad1fd432fd2f7
Fabricio C Zuardi <fabricio@fabricio.org>
2016-05-11 13:21:06 -0300

    fake-clickbus-api as a dependency and a test for it

---
+++ test/clickbus.js
@@ -0,0 +1,16 @@
+import test from 'ava';
+
+const request = require('request-promise');
+const fakeClickBus = require('fake-clickbus-api');
+
+const CLICKBUS_URL = process.env.CLICKBUS_URL ||
+'https://api-evaluation.clickbus.com.br/api/v1';
+
+test('Clickbus places api returns a json with items.', t => {
+    const scope = fakeClickBus(CLICKBUS_URL);
+    const url = CLICKBUS_URL + '/places';
+    return request(url).then(body => {
+        const result = JSON.parse(body);
+        t.truthy(result.items);
+    });
+});
