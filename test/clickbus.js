f14085e652ff44b2cad07fb94646c18ce2334995
Fabricio C Zuardi <fabricio@fabricio.org>
2016-05-12 01:46:20 -0300

    lint

--- test/clickbus.js
+++ test/clickbus.js
@@ -8,7 +8,8 @@ const CLICKBUS_URL = process.env.CLICKBUS_URL ||
 
 test('Clickbus places api returns a json with items.', t => {
     const scope = fakeClickBus(CLICKBUS_URL);
-    const url = CLICKBUS_URL + '/places';
+    t.truthy(scope);
+    const url = `${CLICKBUS_URL}/places`;
     return request(url).then(body => {
         const result = JSON.parse(body);
         t.truthy(result.items);
