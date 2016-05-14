88f8957fafc4aa82120e21e40a425675eb5e49e3
Fabricio C Zuardi <fabricio@fabricio.org>
2016-05-14 17:57:17 -0300

    fix insult mishandling

--- src/routes/insult.js
+++ src/routes/insult.js
@@ -2,7 +2,7 @@ import { replies } from '../../replies';
 import { WitDriver } from 'calamars';
 
 const routes = [[
-    outcomes => WitDriver.getEntityValue(outcomes, 'insult') !== undefined,
+    outcomes => WitDriver.getEntityValue(outcomes, 'insult') !== null,
     replies.insult
 ]];
 export default routes;
