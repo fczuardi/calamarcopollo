43c1ec0384901aaa8554a090f7c6d7a0460e819f
Fabricio C Zuardi <fabricio@fabricio.org>
2016-06-22 16:27:24 -0300

    only match a faq entity if there is an answer for that value, fix #104

--- src/routes/faq.js
+++ src/routes/faq.js
@@ -2,7 +2,10 @@ import { replies } from '../../replies';
 import { WitDriver } from 'calamars';
 
 const routes = [[
-    outcomes => WitDriver.getEntityValue(outcomes, 'faq') !== null,
+    outcomes => (
+        WitDriver.getEntityValue(outcomes, 'faq') !== null &&
+        replies.faq(WitDriver.getEntityValue(outcomes, 'faq'))
+    ),
     outcomes => replies.faq(WitDriver.getEntityValue(outcomes, 'faq'))
 ]];
 export default routes;
