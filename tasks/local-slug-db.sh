6f395f2403d035f86a7436fb2e9b5fcb4bd604df
Fabricio C Zuardi <fabricio@fabricio.org>
2016-06-28 14:35:04 -0300

    tweak slug generation script to make "barra funda" be an alias

--- tasks/local-slug-db.sh
+++ tasks/local-slug-db.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 
-jq '[.items[] | {slug, name: (.name|sub(" \\-.*"; "")), alias: (.name|sub(" \\-.*"; "")|sub("\\,..."; "")|ascii_downcase)}]' \
+jq '[.items[] | {slug, name: (.name|sub(" \\- TODOS"; ""; "i")), alias: (.name|sub(" \\- TODOS"; ""; "i")|sub("\\,..."; "")|sub(".* \\- "; "")|ascii_downcase)}]' \
 data/places.json \
 > data/slugdb.json
